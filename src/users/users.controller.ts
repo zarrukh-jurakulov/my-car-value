bffb18510ece9262800e67c45133dd2ff5d43c5d
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2026-04-08 20:49:15 +0500

    change

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,9 +7,13 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -20,9 +24,16 @@ export class UsersController {
     return this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('404 - Not Found');
+    }
+
+    return user;
   }
 
   @Get()
@@ -33,5 +44,10 @@ export class UsersController {
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
   }
 }
