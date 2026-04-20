0219c509241d9d53d48fbb9a9d33e684fb7f5ef8
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2026-04-20 20:20:15 +0500

    change

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
+
+// const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['qwerty'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
